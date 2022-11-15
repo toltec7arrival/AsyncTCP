5b15806b716f4f945439b4f101a35a9858a2d749
toltec7arrival <mrfaragi@gmail.com>
2022-11-15 22:52:32 +0100

    Revert "Test WDT"
    This reverts commit fd8b95b666e02d879db657f53714a0938d99705c.

--- src/AsyncTCP.h
+++ src/AsyncTCP.h
@@ -31,13 +31,10 @@ extern "C" {
 }
 
 //If core is not defined, then we are running in Arduino or PIO
-//#ifndef CONFIG_ASYNC_TCP_RUNNING_CORE
-//#define CONFIG_ASYNC_TCP_RUNNING_CORE -1 //any available core
-//#define CONFIG_ASYNC_TCP_USE_WDT 1 //if enabled, adds between 33us and 200us per event
-//#endif
-
-#define CONFIG_ASYNC_TCP_RUNNING_CORE 0
-#define CONFIG_ASYNC_TCP_USE_WDT 0
+#ifndef CONFIG_ASYNC_TCP_RUNNING_CORE
+#define CONFIG_ASYNC_TCP_RUNNING_CORE -1 //any available core
+#define CONFIG_ASYNC_TCP_USE_WDT 1 //if enabled, adds between 33us and 200us per event
+#endif
 
 //If stack size is not defined, then defaule stack size is 6144
 #ifndef CONFIG_ASYNC_TCP_STACK_SIZE
